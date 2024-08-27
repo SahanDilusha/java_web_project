ce373329d6f1d21159065480427d26a2d9256574
SahanDilusha <sahancodelygroup@gmail.com>
2024-08-27 12:42:07 +0530

    logout done

--- src/java/controller/User_Logout.java
+++ src/java/controller/User_Logout.java
@@ -10,6 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "User_Logout", urlPatterns = {"/User_Logout"})
 public class User_Logout extends HttpServlet {
 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getSession().invalidate();
+    }
+
+    
 
 
 }
