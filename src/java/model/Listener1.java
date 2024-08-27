b3b209cac9066a1853a46d89de6515bf944d82ad
SahanDilusha <sahancodelygroup@gmail.com>
2024-08-27 11:14:56 +0530

    create Listener1 and user bean

--- src/java/model/Listener1.java
+++ src/java/model/Listener1.java
@@ -1,5 +1,23 @@
 package model;
 
-public class Listener1 {
+import java.util.ArrayList;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class Listener1 implements ServletContextListener{
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+        ArrayList<User> userList = new ArrayList<User>();
+        sce.getServletContext().setAttribute("userList", userList);
+        
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
     
 }
